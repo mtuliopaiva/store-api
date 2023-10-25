aa20a9cc832170bbce25262cd558a4005545908f
Marco Paiva <tulio.paiva10@gmail.com>
2023-10-24 21:12:12 -0300

    Configuration of pg database - incomplete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
 import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule, 
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal:true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
   controllers: [],
   providers: [],
 })
